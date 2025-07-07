d4e66ed1e44bfd7ec62a921149988b5a777b7dd1
GestorEterno <eternalgenesisgestor@gmail.com>
2025-07-07 04:20:42 -0300

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,10 +1,14 @@
-// -----------------------------
-// NAVBAR JS - AVALON
-// -----------------------------
+/**
+ * NAVBAR.JS - Avalon Creators
+ * Versión limpia y minimalista
+ */
 
-// Redirigir al index al hacer clic en el logo
+// Espera a que el DOM esté cargado
 document.addEventListener('DOMContentLoaded', () => {
+  // Selecciona el logo por clase
   const logo = document.querySelector('.logo');
+
+  // Al hacer clic, navega a index.html
   if (logo) {
     logo.addEventListener('click', () => {
       window.location.href = 'index.html';
